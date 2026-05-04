8523d162c94f28cbb253c6db7e0cfea067a5dabb
DadeOrsu <dade.orsu@gmail.com>
2026-05-04 19:01:16 +0200

    Now it uses the @once_at decorater to schedule the link failure at the right time

--- eclypse/network/fault_injection.py
+++ eclypse/network/fault_injection.py
@@ -1,26 +1,19 @@
-from eclypse.workflow.event import EclypseEvent
-from eclypse.workflow.trigger import CascadeTrigger
+from network import Network
 
+from eclypse.workflow.event import once_at
 
-class FaultInjectionEvent(EclypseEvent):
-    def __init__(self):
-        super().__init__(
-            name="fault_injection",
-            event_type="infrastructure",
-            triggers=[CascadeTrigger("step")]
-        )
-        self.current_tick = 0
-
-    def __call__(self, infr, placement_view, **kwargs):
-        self.current_tick += 1
-
-        if self.current_tick == 50:
-            print(f"\n[💥 GUASTO] Tick {self.current_tick}: Rottura del link Gateway -> R1!")
-            infr.remove_edge("Gateway", "R1")
-            
-            if hasattr(infr, "_paths"):
-                infr._paths.clear()
-
-            if hasattr(infr, "install_shortest_path_routes"):
-                infr.install_shortest_path_routes()
-                print("[OSPF] Rotte ricalcolate d'emergenza su R2.\n")
+# tick 50 = 50 seconds
+@once_at(sim_seconds=50, event_type="infrastructure", name="fault_injection")
+def FaultInjectionEvent(infr: Network, placement_view, **kwargs):
+    """
+    Event that fires EXACTLY at 50 simulation seconds (Tick 50).
+    Removes the link and forces an OSPF recalculation.
+    """
+    print("\n[💥 FAULT] Time 50s (Tick 50): Gateway->R1 link broken!")
+    # remove physically the link from the infrastructure graph. This simulates a real failure and allows us to test the OSPF reaction.
+    infr.remove_edge("Gateway", "R1")
+    # empty the infrastructure paths cache to force the recalculation of routes.
+    infr._paths.clear()
+    # recalculate the routes
+    infr.install_shortest_path_routes()
+    print("[OSPF] Rotte ricalcolate d'emergenza su R2.\n")
