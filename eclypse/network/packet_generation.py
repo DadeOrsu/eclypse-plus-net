db58ca36399aef016bc9a7dcf0f5eaebf7074a1c
DadeOrsu <dade.orsu@gmail.com>
2026-05-04 19:04:21 +0200

    Added typing hints to PacketGenerationEvent and cleaned up the code.

--- eclypse/network/packet_generation.py
+++ eclypse/network/packet_generation.py
@@ -1,26 +1,19 @@
 from eclypse.workflow.event import EclypseEvent
 from eclypse.workflow.trigger import CascadeTrigger
-
+from network_application import NetworkAwareApplication
+from network import Network
 
 class PacketGenerationEvent(EclypseEvent):
     def __init__(self):
-        # Il nuovo modo per sostituire @event(event_type="application", activates_on="step")
         super().__init__(
             name="packet_generation",
             event_type="application",
             triggers=[CascadeTrigger("step")]
         )
 
-    # The framework injects automatically (app, placement, infra)
-    def __call__(self, app, placement, infra, **kwargs):
-
+    def __call__(self, app: NetworkAwareApplication, placement, infra: Network, **kwargs):
         # The application takes care of the counting of the ticks, so we just need to call the traffic generation method
-        if not hasattr(app, "current_tick"):
-            app.current_tick = 0
         app.current_tick += 1
-
-        if hasattr(app, "generate_traffic_for_tick"):
-            app.generate_traffic_for_tick(app.current_tick)
-            print(f"[DEBUG GEN] Tick {app.current_tick}: App '{app.id}' has generated {len(app.generated_packets)} packets.")
-
+        app.generate_traffic_for_tick(app.current_tick)
+        print(f"[DEBUG GEN] Tick {app.current_tick}: App '{app.id}' has generated {len(app.generated_packets)} packets.")
         return {"packets_generated": len(app.generated_packets) if hasattr(app, 'generated_packets') else 0}
