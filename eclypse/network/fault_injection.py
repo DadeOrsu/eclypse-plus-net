a6bf5d699258b835fd178f3cc97d03bc8e462b58
DadeOrsu <dade.orsu@gmail.com>
2026-05-15 16:35:56 +0200

    Updated fault injection to simulate link failure without forcing route recalculation because it's triggered by remove_edge method.

--- eclypse/network/fault_injection.py
+++ eclypse/network/fault_injection.py
@@ -12,7 +12,3 @@ def FaultInjectionEvent(infr: Network, placement_view, **kwargs):
     infr.logger.warning("\nstep 50: Gateway->R1 link broken! Routes recalculated.\n")
     # remove physically the link from the infrastructure graph. This simulates a real failure and allows us to test the OSPF reaction.
     infr.remove_edge("Gateway", "R1")
-    # empty the infrastructure paths cache to force the recalculation of routes.
-    infr._paths.clear()
-    # recalculate the routes
-    infr.install_shortest_path_routes()
