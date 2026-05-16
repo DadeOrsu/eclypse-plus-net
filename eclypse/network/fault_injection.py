358eb3ceefbdf905a4560b22a01f6e833dac1749
DadeOrsu <dade.orsu@gmail.com>
2026-05-16 18:22:23 +0200

    Added docstring to FaultInjectionEvent and removed an unnecessary comment.

--- eclypse/network/fault_injection.py
+++ eclypse/network/fault_injection.py
@@ -1,14 +1,12 @@
 from network import Network
-
 from eclypse.workflow.event import once_at
 
 
 @once_at(step=50, event_type="infrastructure", name="fault_injection")
 def FaultInjectionEvent(infr: Network, placement_view, **kwargs):
-    """
-    Event that fires EXACTLY at step 50.
+    """Event that fires EXACTLY at step 50.
+
     Removes the link and forces an OSPF recalculation.
     """
-    infr.logger.warning("\nstep 50: Gateway->R1 link broken! Routes recalculated.\n")
-    # remove physically the link from the infrastructure graph. This simulates a real failure and allows us to test the OSPF reaction.
+    infr.logger.warning("step 50: Gateway->R1 link broken! Routes recalculated.\n")
     infr.remove_edge("Gateway", "R1")
